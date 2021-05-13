12955aab467ed888ffd141d655c86c7c4c66766c
Bianca <biancancanal@gmail.com>
2021-05-13 10:41:43 -0500

    Complete with Drafts

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+def reverse_each_word(array) 
+    array.split.collect {|word| word.reverse}.join(" ")
+end
+
+
+
+# def reverse_each_word(array) 
+#     reverse_array = []
+#     array.each do |word| 
+#         reverse_array >> word.reverse.join("") 
+#     end
+#     reverse_array
+# end
+
+
+# def reverse_each_word(array) 
+#     array.collect do |word| 
+#         word.reverse.join("") 
+#     end
+# end
